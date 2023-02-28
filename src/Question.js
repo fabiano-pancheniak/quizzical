f01ce18bf0288be7dd9e70453228bc0de007eb15
Fabiano <fabiano.pancheniakfilho@gmail.com>
2023-02-27 23:19:43 -0300

    v1

--- src/Question.js
+++ src/Question.js
@@ -1,13 +1,32 @@
-export default function Question(){
+import React from "react"
+
+export default function Question(props){
+
+    const answersElements = props.question.answers.map((element, index) => {    
+        let bgColor = ''
+        if(atob(element) === props.question.selected){
+            bgColor = 'selected'
+        }
+
+        if((atob(element) === props.question.selected) && props.showAnswers) {
+            bgColor = 'wrong'
+        }
+        if((atob(props.question.correctAnswer) === atob(element)) && props.showAnswers){
+            bgColor = 'correct'
+        } 
+
+        return(
+            <button className={bgColor} onClick={(event) => props.handleClickAnswer(event.target.id, event.target.value)} value={atob(element)} id={props.question.id} key={index}>{atob(element)}</button>
+        )   
+    })
+
+   
     return(
         <div className="question-container">
-            <h4>How would one say goodbye in Spanish?</h4>
-            <ul>
-                <li className="correct">Adios</li>
-                <li className="selected">Hola</li>
-                <li className="wrong"> Au Revoir</li>
-                <li>Salir</li>
-            </ul>
+            <h4>{atob(props.question.questionTitle)}</h4>
+                <div className="options">
+                    {answersElements}
+                </div>
             <hr />
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Start from './Start.js'
 import Quiz from './Quiz.js';
 import './css/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Quiz />
-  </React.StrictMode>
 );
 
